ceffcf60832ea36176f0d1b805a3d20b7cbcf93c
Jaden Hoenes <jaden.hoenes@gmail.com>
2023-01-08 21:07:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const clientId = "58482e5f3d5c441e9288469eb713ca2e";
-const clientSecret = "62fbf749e5794b4db12665b3342926ac";
 const redirectUri = "https://www.jaden-hoenes.github.io/spotifyEmbed/";
 
 console.log('loaded');
